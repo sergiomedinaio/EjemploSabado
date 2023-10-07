edb30f3a635f9515ba155a6ebc97556fec9f5fe0
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-07 17:29:57 -0300

    feat: add toast on click

--- app/src/main/java/ar/edu/davinci/ejemplosabado/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/ejemplosabado/MainActivity.java
@@ -1,5 +1,4 @@
 package ar.edu.davinci.ejemplosabado;
-
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -7,6 +6,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,10 +23,15 @@ public class MainActivity extends AppCompatActivity {
 
         contentButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
+            public void onClick(View button) {
                 TextView contentTitle = findViewById(R.id.contentTitle);
                 contentTitle.setText(R.string.contentButtonTextOnClick);
                 contentTitle.setBackgroundColor(+R.color.red_500);
+
+                CharSequence msg = "Estado actualizado";
+                //button.getContext() devuelve el MainActivity
+                Toast toast = Toast.makeText(button.getContext(), msg, Toast.LENGTH_SHORT);
+                toast.show();
             }
         });
 
