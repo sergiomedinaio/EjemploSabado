3d6e2e7304b5289b341e0b6a4ab5b8dfe9072f10
sergiomedinaio <63879459+sergiomedinaio@users.noreply.github.com>
2023-10-07 16:46:00 -0300

    feat: update text content on click

--- app/src/main/java/ar/edu/davinci/ejemplosabado/MainActivity.java
+++ app/src/main/java/ar/edu/davinci/ejemplosabado/MainActivity.java
@@ -3,6 +3,9 @@ package ar.edu.davinci.ejemplosabado;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button contentButton = findViewById(R.id.contentButton);
+        contentButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView contentTitle = findViewById(R.id.contentTitle);
+                contentTitle.setText(R.string.contentButtonTextOnClick);
+            }
+        });
+
     }
 }
